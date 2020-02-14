12a5c0ef1079e35a176f9689c60edb733f4b504a
krengt <krengt@live.jp>
2020-02-14 21:41:22 +0900

    add WordPress settings

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,21 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: process.env.SITE_NAME,
+  siteUrl: process.env.SITE_URL,
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: process.env.WORDPRESS_CONTAINER_URL || process.env.WORDPRESS_URL,
+        apiBase: 'wp-json',
+        typeName: 'WordPress',
+        perPage: 100,
+        concurrent: 10
+      }
+    }
+  ],
+  templates: {
+    WordPressPost: '/:year/:month/:day/:slug'
+  }
 }
--- gridsome.server.js
+++ gridsome.server.js
@@ -6,8 +6,61 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async ({ getCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    const http = require('http')
+    const fs = require('fs')
+    const mkdirp = require('mkdirp')
+    const path = require('path')
+    const url = require('url')
+    const rimraf = require('rimraf')
+
+    // Fetch upload files from headless CMS
+    const attachments = getCollection('WordPressAttachment')
+    rimraf.sync('./static/uploads')
+
+    if (attachments) {
+      for (const attachment of attachments.findNodes()) {
+        if (attachment.post) {
+          const parsedUrl = url.parse(attachment.sourceUrl)
+          const hostRegexp = new RegExp(`${parsedUrl.protocol}//${parsedUrl.host}`, 'g')
+          const wordpressUrl = process.env.WORDPRESS_CONTAINER_URL || process.env.WORDPRESS_URL
+          const downloadUrl = attachment.sourceUrl.replace(hostRegexp, wordpressUrl)
+
+          const dirName = path.dirname(parsedUrl.pathname).replace(/\/wp-content/g, '')
+          const fileName = path.basename(parsedUrl.pathname)
+          const filePath = `${dirName}/${fileName}`
+
+          const assetsFilePath = `${api.config.pathPrefix}${filePath}` 
+          const staticFilePath = `./static${assetsFilePath}` 
+
+          mkdirp.sync(path.dirname(staticFilePath))
+
+          http.get(downloadUrl, (response) => {
+            response.pipe(fs.createWriteStream(staticFilePath))
+          })
+
+          attachment.assetsFilePath = assetsFilePath
+        }
+      }
+    }
+
+    // Remove origin hostname
+    const posts = getCollection('WordPressPost')
+    if (posts) {
+      for (const post of posts.findNodes()) {
+        post.originalContent = post.content
+
+        const attributesRegExp = new RegExp(/["'](.*?)["']/g)
+        const uploadPathRegExp = new RegExp(`${process.env.WORDPRESS_URL}/wp-content`, 'g')
+
+        const replacedContent = post.content.replace(attributesRegExp, attributeValue => {
+          return attributeValue.replace(uploadPathRegExp, `${api.config.pathPrefix}`)
+        })
+
+        post.content = replacedContent
+      }
+    }
   })
 
   api.createPages(({ createPage }) => {
